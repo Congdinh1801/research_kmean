b4d7e2d62f1e77ce2564f9f9f281eb92c14f38ae
Congdinh1801 <Congdinh1801@yahoo.com>
2021-09-01 16:27:35 -0500

    Add project files.

---
+++ research_kmean/testmain.cpp
@@ -0,0 +1,11 @@
+#include <time.h>       /* clock_t, clock, CLOCKS_PER_SEC */
+#include <iostream> 
+
+using namespace std;
+
+int main()
+{
+	cout << "hello world" << endl;
+	cout << "hello world2";
+	return 0;
+}
